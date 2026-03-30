726a6e93ee13fba044be70aa8f24c750d711ca80
raposinha <elena@raposinha.dev>
2026-03-30 16:10:11 +0200

    adds test of abbonamento

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,13 @@
 import dao.AcquistoDAO;
 import dao.DistributoreAutomaticoDao;
+import entities.Abbonamento;
 import entities.Biglietto;
 import entities.DistributoreAutomatico;
 import enumerated.LocationAcquisto;
+import enumerated.Periodo;
 import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW5-mezziPubblici");
@@ -12,15 +16,16 @@ public class Application {
         EntityManager em = emf.createEntityManager();
         AcquistoDAO acquistoDAO = new AcquistoDAO(em);
 
-        Biglietto b1 = new Biglietto(LocationAcquisto.AUTOMATICO);
         DistributoreAutomaticoDao distributoreAutomaticoDao = new DistributoreAutomaticoDao(em);
         DistributoreAutomatico d1 = new DistributoreAutomatico(true);
         Biglietto b1 = new Biglietto(LocationAcquisto.AUTOMATICO,d1);
         distributoreAutomaticoDao.save(d1);
         acquistoDAO.save(b1);
+DistributoreAutomatico foundD = distributoreAutomaticoDao.findById(1223456);
+        acquistoDAO.save(b1);
 
-        //acquistoDAO.save(b1);
+
+        Abbonamento ab = new Abbonamento(LocationAcquisto.RIVENDITORE, Periodo.MENSILE, LocalDate.now(), "Elena", "Blabla", LocalDate.of(1900,01,01), foundD);
+        acquistoDAO.save(ab);
     }
-
-
 }
