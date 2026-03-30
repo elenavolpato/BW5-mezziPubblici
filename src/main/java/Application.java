65f50fbbe5ed5715592fb34d65143f18be921cd9
faaizaan <fsdar3108@gmail.com>
2026-03-30 15:06:29 +0200

    entity-tratta

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,18 @@
+import entities.Tratta;
 import jakarta.persistence.*;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("BW5-mezziPubblici");
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }
+
+
 
 }
